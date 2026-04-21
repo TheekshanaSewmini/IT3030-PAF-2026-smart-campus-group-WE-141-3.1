cf2afec45a4204332ae7667ae362565b48968421
Damsi119 <nayanama.d2000@gmail.com>
2026-04-21 16:12:56 +0530

    create forgotpassword enity and create recoverychanel enum and complete fogotpasswrod enity

---
+++ smart-campus/backend/src/main/java/com/smartcampus/smart_campus/enums/RecoveryChannel.java
@@ -0,0 +1,7 @@
+package com.smartcampus.smart_campus.enums;
+
+public enum RecoveryChannel {
+    EMAIL,         // primary email
+    BACKUP_EMAIL,  // tempEmail
+    PHONE          // phone number
+}
