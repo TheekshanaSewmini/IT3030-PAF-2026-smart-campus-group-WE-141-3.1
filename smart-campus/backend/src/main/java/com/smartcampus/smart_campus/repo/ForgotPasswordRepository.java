25243dc1b17fc71fa8850c93cc549f75ea82064d
Damsi119 <nayanama.d2000@gmail.com>
2026-04-21 16:34:20 +0530

    create forgotpasswrod and complete both userrepo and forgotpasswrod

---
+++ smart-campus/backend/src/main/java/com/smartcampus/smart_campus/repo/ForgotPasswordRepository.java
@@ -0,0 +1,15 @@
+package com.smartcampus.smart_campus.repo;
+
+import com.smartcampus.smart_campus.entities.ForgotPassword;
+import com.smartcampus.smart_campus.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, Integer> {
+
+
+
+
+    Optional<ForgotPassword> findByUser(User user);
+}
