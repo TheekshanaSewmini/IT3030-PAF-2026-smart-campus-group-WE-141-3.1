5a0895816e2bde82cf801a8c3a8524414c374bc1
Damsi119 <nayanama.d2000@gmail.com>
2026-04-21 16:37:35 +0530

    create and complete securityconfig class

---
+++ smart-campus/backend/src/main/java/com/smartcampus/smart_campus/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.smartcampus.smart_campus.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private  final JWTAuthFilter jwtAuthFilter;
+    private  final AuthenticationProvider authenticationProvider;
+
+    // Security filter chain
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration crf = new CorsConfiguration();
+                    crf.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
+                    crf.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+                    crf.setAllowedHeaders(List.of("*"));
+                    crf.setAllowCredentials(true);
+                    crf.setMaxAge(3600L);
+                    return crf;
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers(
+                                "/auth/**",
+                                "/forgotpass/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
+
